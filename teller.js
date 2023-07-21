de00e3dc4a4b7c49edbf126433564530f548f20c
dfralan <80047722+dfralan@users.noreply.github.com>
2023-07-20 22:24:10 -0300

    Update teller.js

--- teller.js
+++ teller.js
@@ -1,6 +1,6 @@
 /*!
-  * Teller v2.0.0 (https://github.com/dfralan/Teller)
-  * Copyright 2021-2023 The Teller Authors.
+  * Teller v1.0.0 (https://github.com/dfralan/Teller)
+  * Copyright 2021-2022 The Teller Authors.
   * Licensed under GNU GENERAL PUBLIC LICENSE (https://github.com/dfralan/Teller/blob/main/LICENSE)
   */
 function kiujdgrsghsss(){
@@ -1831,14 +1831,14 @@ function kiujdgrsghsss(){
             //Zero Zone
             var localDate = new Date();
             var zeroDate = localDate.toUTCString();
-            zeroTime = ((zeroDate.split(" "))[4]).split(":");
-            zeroInSec = (zeroTime[0]*3600)+(zeroTime[1]*60);
-            a = h.slice(0, 5); a1 = a.split(":"); a2 = (a1[0]*3600)+(a1[1]*60); //openhour
-            b = h.slice(6, 11); b1 = b.split(":"); b2 = (b1[0]*3600)+(b1[1]*60); //endhour
-            pol = h.slice(12, 13)//operator (+ or -)
-            c = h.slice(13, 18); c1 = c.split(":"); c2 = (c1[0]*3600)+(c1[1]*60);//gmt zone
-            oh = 0;
-            eh = 0;
+            var zeroTime = ((zeroDate.split(" "))[4]).split(":");
+            var zeroInSec = (zeroTime[0]*3600)+(zeroTime[1]*60);
+            let a = h.slice(0, 5); a1 = a.split(":"); a2 = (a1[0]*3600)+(a1[1]*60); //openhour
+            let b = h.slice(6, 11); b1 = b.split(":"); b2 = (b1[0]*3600)+(b1[1]*60); //endhour
+            let pol = h.slice(12, 13)//operator (+ or -)
+            var c = h.slice(13, 18); c1 = c.split(":"); c2 = (c1[0]*3600)+(c1[1]*60);//gmt zone
+            let oh = 0;
+            let eh = 0;
             getOhEh()
             function getOhEh(){
                 if(pol=="+"){oh = a2-c2; eh = b2-c2}
